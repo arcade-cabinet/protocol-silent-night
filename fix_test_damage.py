be4b49797eedb952eb7a5e354d7acaae49d1f33c
Jon Bogaty <jon@jonbogaty.com>
2026-04-11 01:12:39 -0500

    chore: clean up python scripts (#186)

--- fix_test_damage.py
+++
@@ -1,7 +0,0 @@
-with open('test/unit/test_player_damage_handler.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace('"class": {"contact_damage_reduction": 0.0}', '"class": ClassResource.new()')
-
-with open('test/unit/test_player_damage_handler.gd', 'w') as f:
-    f.write(content)
