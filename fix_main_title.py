1bb66449fd94289a54aa37ff1fa76b1ca110a90b
Jon Bogaty <jon@jonbogaty.com>
2026-04-11 00:16:56 -0500

    chore: clean up temp python scripts (#184)

--- fix_main_title.py
+++
@@ -1,7 +0,0 @@
-with open('scripts/main.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace("var start_screen: PanelContainer:\n\tget: return ui_mgr.start_screen", "var title_screen: PanelContainer:\n\tget: return ui_mgr.title_screen\nvar start_screen: PanelContainer:\n\tget: return ui_mgr.start_screen")
-
-with open('scripts/main.gd', 'w') as f:
-    f.write(content)
--- fix_test_main.py
+++
@@ -1,7 +0,0 @@
-with open('test/component/test_main_scene.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace('assert_bool(main.start_screen.visible).is_true()', 'assert_bool(main.title_screen.visible).is_true()')
-
-with open('test/component/test_main_scene.gd', 'w') as f:
-    f.write(content)
