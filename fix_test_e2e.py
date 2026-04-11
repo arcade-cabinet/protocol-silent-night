52a5bf6ad3bd8ecddb7655cc6dcac1cb3cb70764
Jon B <jon@jonbogaty.com>
2026-04-11 00:16:49 -0500

    chore: clean up temp python scripts

--- fix_test_e2e.py
+++
@@ -1,7 +0,0 @@
-with open('test/e2e/test_full_session_flow.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace('assert_bool(main.start_screen.visible).is_true()', 'assert_bool(main.title_screen.visible).is_true()')
-
-with open('test/e2e/test_full_session_flow.gd', 'w') as f:
-    f.write(content)
--- fix_ui_builder4.py
+++
@@ -1,7 +0,0 @@
-with open('scripts/ui_builder.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace('return {"screen": start_screen, "classes_box": classes_box, "radar_canvas": radar_canvas}', 'return {"screen": start_screen, "classes_box": classes_box, "radar_canvas": radar_canvas, "select_btn": select_btn}')
-
-with open('scripts/ui_builder.gd', 'w') as f:
-    f.write(content)
