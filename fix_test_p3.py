e45aac3728157d38d6067dce56eb2455258d8ab6
Jon Bogaty <jon@jonbogaty.com>
2026-04-11 18:08:10 -0500

    chore: remove final scripts (#192)

--- fix_test_p3.py
+++
@@ -1,7 +0,0 @@
-with open('test/unit/test_p3_polish.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace('found = True', 'found = true')
-
-with open('test/unit/test_p3_polish.gd', 'w') as f:
-    f.write(content)
