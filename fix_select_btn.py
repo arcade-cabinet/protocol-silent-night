0359d3da19c919f32b973656329f1ca3e23a9c06
Jon Bogaty <jon@jonbogaty.com>
2026-04-11 01:17:20 -0500

    chore: remove temp script (#188)

--- fix_select_btn.py
+++
@@ -1,7 +0,0 @@
-with open('scripts/ui_manager.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace("func refresh_start_screen(save_manager: Node, on_class_pressed: Callable, present_defs: Dictionary = {}) -> void:\n\ttitle_screen.visible = true\n\tstart_screen.visible = false\n\tif difficulty_panel != null:\n\t\tdifficulty_panel.visible = false", "func refresh_start_screen(save_manager: Node, on_class_pressed: Callable, present_defs: Dictionary = {}) -> void:\n\ttitle_screen.visible = true\n\tstart_screen.visible = false\n\tif select_button != null:\n\t\tselect_button.disabled = true\n\tif difficulty_panel != null:\n\t\tdifficulty_panel.visible = false")
-
-with open('scripts/ui_manager.gd', 'w') as f:
-    f.write(content)
