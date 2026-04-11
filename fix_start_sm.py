18d4594c9df3f31f3ea2a8c6bc37b61407ab17b2
Jon B <jon@jonbogaty.com>
2026-04-11 18:08:03 -0500

    chore: remove final scripts

--- fix_start_sm.py
+++
@@ -1,7 +0,0 @@
-with open('scripts/game_manager.gd', 'r') as f:
-    content = f.read()
-
-content = content.replace('\tvar start_sm: Node = main._save_manager()\n\tif start_sm != null:\n\t\tstart_sm.record_run_start()\n', '', 1)
-
-with open('scripts/game_manager.gd', 'w') as f:
-    f.write(content)
